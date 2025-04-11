5ae6c6bdeab0978c62eca1b90a0c97b0fa791d74
Tarun Kishor Patil <87746946+TarunPatil001@users.noreply.github.com>
2025-04-11 14:56:13 +0530

    files updated

--- src/Components/SearchBox/index.jsx
+++ src/Components/SearchBox/index.jsx
@@ -18,7 +18,7 @@ const SearchBox = (props) => {
     <div className="w-full h-auto border bg-[#f1f1f1] relative rounded-md">
     <IoSearchOutline className="top-[10px] left-[5px] text-[20px] absolute z-40 pointer-events-none opacity-80" />
     <input
-{/*       autoFocus */}
+      // autoFocus
       type="text"
       className="w-full h-[40px] border border-[rgba(0,0,0,0.1)] bg-[#f1f1f1] p-2 focus:outline-none focus:border-[rgba(0,0,0,0.5)] pl-8 rounded-md"
       placeholder={`Search by ${props?.searchName}...`}
